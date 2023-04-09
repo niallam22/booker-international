d2e1cacc4e46a68e30ab936e56062110dbc94649
Niall Moore <niallam22@gmail.com>
2023-04-09 08:31:20 +0530

    fixed schedule

--- public/js/main.js
+++ public/js/main.js
@@ -14,34 +14,38 @@ window.onload = function(){
 }
 // !!!!!updateOptions() is called when the datepicker value changes, but datepicker.value is used to create a new Date object without checking if it is valid. If datepicker.value is an empty string or an invalid date format, this will cause an error. add a check to ensure that datepicker .value is a valid date before creating a new Date object with it. or a try catch block to catch the error
 const updateOptions = () => {
-  // Generate array of dates for the selected date plus up to 7 more days
+  // Generate array of dates for the selected date, up to 7 days
   const selectedDate = new Date(`${datepicker.value} UTC`);
   const days = [];
+  const scheduleRaw =[]
   for (let i = 0; i < dayRange; i++) {
-    const date = new Date(selectedDate.getTime() + i * 24 * 60 * 60 * 1000);
+    const date = new Date(selectedDate.getTime() + i * 24 * 60 * 60 * 1000); 
     days.push(date);
-  }
+    const hours = [];
 
-  // Generate array of time slots based on earliest and latest times
-  const hours = [];
-  const startHour = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate(), earliestTime);
-  const endHour = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate(), latestTime);
-  for (let i = 0; i <= 23; i++) {
-    const time = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate(), i);
-    if (time >= startHour && time <= endHour) {
-      hours.push(time);
+    // Generate array of time slots based on earliest and latest times
+    const startHour = new Date(date.getFullYear(), date.getMonth(), date.getDate(), earliestTime);
+    const endHour = new Date(date.getFullYear(), date.getMonth(), date.getDate(), latestTime);
+    for (let i = 0; i <= 23; i++) {
+      const time = new Date(date.getFullYear(), date.getMonth(), date.getDate(), i);
+      if (time >= startHour && time <= endHour) {
+        hours.push(time);
+      }
     }
+
+    scheduleRaw.push([ date, hours]);
+
   }
 
   // Clear the current options for the schedule
   const schedule = document.querySelector('#schedule');
   schedule.innerHTML = '';
 
-  //create table header 
+  //Table header 
   const tableHeadRow = document.createElement('tr')
-  days.forEach(day => {
-    const dayOfWeek = day.toLocaleDateString('en-US', { weekday: 'long' });
-    const dayOfMonth = day.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit' });
+  scheduleRaw.forEach((day) => {
+    const dayOfWeek = day[0].toLocaleDateString('en-US', { weekday: 'long' });
+    const dayOfMonth = day[0].toLocaleDateString('en-US', { month: '2-digit', day: '2-digit' });
     const dayId = dayOfWeek.toLowerCase();
 
     const dayOption = document.createElement('th');
@@ -52,29 +56,33 @@ const updateOptions = () => {
     tableHeadRow.appendChild(dayOption)
   })
 
+  // Table body
   const tableBody = document.createElement('tbody')
-  hours.forEach((hour, index)=>{ //create table row by row (1 row for each hour)
+  scheduleRaw[0][1].forEach((hour, index)=>{ //scheduleRaw format is [[date,[hoursArray]],[date,[hours]], [date,[hours]], ...]
+
+    //create table row by row (1 row for each hour)
     const hourRow = document.createElement('tr')
 
     //populate each column with the input info (ie moving mon-fri add the input field for 9am for each day and then move to the next row and then do the same for 10am etc.).
-    days.forEach(day => {
-      const dayOfWeek = day.toLocaleDateString('en-US', { weekday: 'long' });
-      const dayOfMonth = day.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit' });
-      const dayId = dayOfWeek.toLowerCase();
+    scheduleRaw.forEach((day,i) => {
+      const dayOfWeek = day[0].toLocaleDateString('en-US', { weekday: 'long' });
+
+      const hoursArr = scheduleRaw[i][1]
+      const realHour = hoursArr[index]
 
       //create input field
       const checkbox = document.createElement('input');
       checkbox.setAttribute('type', 'checkbox');
       checkbox.setAttribute('name', 'facilitatorTime');
       checkbox.setAttribute('class', 'timeSlot');
-      checkbox.setAttribute('value', `${dayOfWeek}, ${hour.toISOString()}`)
+      checkbox.setAttribute('value', `${dayOfWeek}, ${realHour.toISOString()}`)
       //append input field to a td element to create a cell in the tabel
       const hourOption = document.createElement('td')
       hourOption.appendChild(checkbox)
 
       //append label to input field so user can see the time
       const label = document.createElement('label');
-      label.textContent = hour.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+      label.textContent = realHour.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
       hourOption.appendChild(label);
 
       hourRow.appendChild(hourOption)
